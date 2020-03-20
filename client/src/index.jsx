6981176846e92fb3e5085b3efc22b38528a27d09
Brandon Gomez <BrandonJosephGomez@gmail.com>
2020-03-20 09:32:48 -0600

    Updated.

---
+++ client/src/index.jsx
@@ -0,0 +1,13 @@
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './components/App.jsx';
+
+// render your App component to your HTML
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+
+
